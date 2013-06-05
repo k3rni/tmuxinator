353e63bb22df6c52101f7d614ab5d733d011badd
Krzysztof Zych <krzysztof.zych@connectmedica.com>
2013-06-05 13:31:20 +0200

    more features

--- tmux.py
+++ tmux.py
@@ -2,9 +2,31 @@
 # encoding: utf-8
 
 from flask import Flask, render_template, url_for
-import json
+import json, subprocess, re
 
 app = Flask(__name__)
+color_support = False
+
+PANE_INFO_RX = re.compile(r'(?P<session>\w+):(?P<window>\w+).(?P<pane>\w+): \[(?P<width>\d+)x(?P<height>\d+)\]')
+def parse_pane_info(line):
+  match = PANE_INFO_RX.match(line)
+  if match:
+    return match.groupdict()
+
+def determine_color_support():
+  major, minor = [int(x) for x in subprocess.check_output('tmux -V', shell=True).split()[-1].split('.')]
+  if major == 1 and minor >= 8:
+    return True
+  elif major > 1:
+    # projecting the future
+    return True
+  else:
+    return False
+
+
+def get_tmux_panes():
+   output = subprocess.check_output('tmux list-panes -a', shell=True).split('\n')
+   return filter(None, [parse_pane_info(row) for row in output])
 
 @app.route('/')
 def index():
@@ -16,13 +38,12 @@ def active_panes():
 
 @app.route('/pane/<name>')
 def read_pane(name):
-  if name == 'foo':
-    return 'Spierdalaj'
-  elif name == 'bar1':
-    return 'Bah'
+  if color_support:
+    return Ansi2HTMLConverter().convert(subprocess.check_output('tmux capture-pane -t %s -e -b 0; tmux save-buffer -b 0 -' % name, shell=True))
   else:
-    return 'No such pane'
+    return "<pre>%s</pre>" % subprocess.check_output('tmux capture-pane -t %s -b 0; tmux save-buffer -b 0 -' % name, shell=True)
 
 if __name__ == "__main__":
   app.debug = True
+  color_support = determine_color_support()
   app.run()
